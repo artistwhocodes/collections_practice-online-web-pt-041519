f9efaa873bff330d0e35178d65214644611f0967
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-26 03:58:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,6 +20,12 @@ end
 
 def swap_elements(array)
   array.sort do | a, b|
-    a[1] <=> b[2] 
+    a[1] <=> b[2]
   end
+end
+
+def reverse_elements(array)
+   array.sort do |a,b|
+      a <==>b
+    end
 end
